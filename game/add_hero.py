4435d36c80e0252d2b4113e458dad609b9dc5b82
daxian <leeoohs@sina.com>
2023-06-01 00:23:44 +0800

    查看英雄信息

--- game/add_hero.py
+++ game/add_hero.py
@@ -13,7 +13,9 @@ print("""
 4. **删除英雄**
 5. **退出系统**
 """)
-
+# 问题：按目前的实现，添加功能和查看都是针对的一个英雄
+# 解决方案：将多个英雄数据保存下来 -> 列表
+hero_list = []
 while True:
     res = input("请输入对应的选项，即可执行对应的操作：")
     if res == "1":
@@ -23,10 +25,15 @@ while True:
         hero_info = {"name": hero_name, "volume": hero_volume, "power": herr_power}
         # 字面量插值，f"普通的字符串{变量}"
         print(f"英雄名字为{hero_name}, 英雄血量为{hero_volume}, 英雄攻击力为{herr_power}")
+        hero_list.append(hero_info)
     elif res == "2":
-        # print(f"查看英雄{hero_info}")
-        # 在字符串中，如果用导流引号，符合内单外双，内双外单的规则
-        print(f"查看英雄{hero_info['name']}血量为{hero_info['volume']}")
+        hero_input_name = input("请输入需要查询的英雄信息：")
+        # 遍历所有的英雄信息，直到匹配到被查询的英雄
+        for i in hero_list:
+            # 直到匹配到被查询的英雄
+            # if 要查询的英雄信息 == 遍历的每英雄的名字
+            if hero_input_name == i['name']:
+                print(f"英雄{hero_input_name}的信息为{i}")
     elif res == "3":
         print("修改英雄信息")
     elif res == "4":
@@ -34,3 +41,5 @@ while True:
     else:
         print("退出系统")
         break
+
+print(f"英雄列表中的所有元素为{hero_list}")
---
+++ game/list_demo.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+# 定义一个列表
+students = []
+print(students)
+# 列表的追加
+students.append("决定书")
+print(students)
+students.append("决定书2")
+students.append("决定书3")
+print(students)
+
+# for 临时变量 in 列表
+for i in students:
+    print(i)
